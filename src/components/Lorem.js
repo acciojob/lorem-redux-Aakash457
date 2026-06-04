bbd5eaec1b55d73d18d9aec5c5223bbbdde425d7
avi-verma-web <aakash.t.yadav@slrtce.in>
2026-06-04 13:26:02 +0530

    update13

--- src/components/Lorem.js
+++ src/components/Lorem.js
@@ -10,6 +10,8 @@ function Lorem() {
     dispatch(fetchLorem());
   }, [dispatch]);
 
+  const post = posts[0];
+
   return (
     <div>
       <h1>A short Naration of Lorem Ipsum</h1>
@@ -18,16 +20,14 @@ function Lorem() {
         your time to Review
       </h4>
 
-      {loading || posts.length === 0 ? (
+      {loading || !post ? (
         <p className="loading">Loading...</p>
       ) : (
         <ul>
-          {posts.map((post) => (
-            <li key={post.id}>
-              <p className="title">Title :{post.title}</p>
-              <p className="body">Body :{post.body}</p>
-            </li>
-          ))}
+          <li key={post.id}>
+            <p className="title">Title :{post.title}</p>
+            <p className="body">Body :{post.body}</p>
+          </li>
         </ul>
       )}
     </div>
